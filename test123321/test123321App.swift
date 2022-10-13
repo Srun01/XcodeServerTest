9d7298e7c2acdf1712073675c6d57261cc6f5171
shepherd <zhoucan007@me.com>
2022-10-13 14:53:11 +0800

    Initial Commit

---
+++ test123321/test123321App.swift
@@ -0,0 +1,17 @@
+//
+//  test123321App.swift
+//  test123321
+//
+//  Created by Shepherd on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct test123321App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
